112f35056a32cad350cde8c7088f87cd2f18a68a
MrBlackGhostt <hemantkumar4213@icloud.com>
2026-02-13 07:37:54 +0530

    fix(sw): Prevent caching of non-GET requests to fix console errors

--- public/sw.js
+++ public/sw.js
@@ -35,6 +35,11 @@ self.addEventListener('fetch', (event) => {
               return response;
             }
 
+            // Only cache GET requests
+            if (event.request.method !== 'GET') {
+              return response;
+            }
+
             // Clone the response
             const responseToCache = response.clone();
 
